86da3379cb8a604c6d54dc7c933ff516120697dd
Asalmerontkd <psrantonio@gmail.com>
2017-07-03 15:07:39 -0500

    Test Timing Oracle

--- schedule.py
+++ schedule.py
@@ -1,42 +1,49 @@
 from datetime import datetime
 from threading import Timer
-import time
+import os
+os.environ["NLS_LANG"] = ".AL32UTF8"
+from bdoracle import Orabd
 
-class MyTemporizador():
+class Recordatorio():
 
-
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
-        self.dia = 0
-        self.hello_world()
+        self.ora = Orabd()
+        self.hilo()
 
-    def hello_world(self):
-        self.dia = self.dia +1
-
-        if self.dia == 2:
-            print("Ya paga culero, es tu último día.")
-        elif self.dia > 2:
-            print("Ya mamaste we, cancelado")
-            self.dia = 0
-        else :
-            print("llevas " + str(self.dia) + " días")
-
-
+    def seconds(self):
         x = datetime.today()
-        # y=x.replace(day=x.day+1, hour=1, minute=0, second=0, microsecond=0)
-        y = x.replace(day=x.day, hour=x.hour, minute=x.minute+1, second=50, microsecond=0)
+        y = x.replace(day=x.day, hour=x.hour, minute=x.minute + 1, second=0, microsecond=0)
         delta_t = y - x
-        secs = delta_t.seconds
-        print(x)
-        print("-----")
-        print(y)
-        print("hello world")
-        print(secs)
-        print(" ")
-        timer = Timer(secs, self.hello_world())
+        secs = delta_t.seconds + 1
+        return secs
+
+    def checkNotify(self):
+        data = self.ora.recordatorio()
+        print("checkNotify . . . . . . . .")
+        for row in data:
+            print(row)
+            self.notifyAlert(row)
+            print("   -------------------------------   ")
+            print("   ")
+        return True
+
+    def notifyAlert(self, row):
+        if self.ora.updateNotificado(row['folio']):
+            print("folio: " + str(row['folio']))
+            print("senderID: "+ str(row['senderID']))
+            print("beneficiario: "+ str(row['beneficiaryName']))
+            print("fecha generada: " + str(row['fechaGenerado']))
+            print("fecha caducidad: " + str(row['fechaCaducidad']))
+            print("El usuario: "+ str(row['userName']) + "ha sido notificado")
+            return True
+        else:
+            return False
+
+    def hilo(self):
+        print(self.seconds())
+        self.checkNotify()
+        timer = Timer(self.seconds(), self.hilo)
         timer.start()
 
-temporizador = MyTemporizador()
-
-
-#Este código se ejecuta en una hora exacta, pero no lo vuelve a ejecutar
+temporizador = Recordatorio()
